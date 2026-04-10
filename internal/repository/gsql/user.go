d288a28d7bdd31dde5ecdca0a0f702622a7dde7d
shuneo <solshuneo@gmail.com>
2026-04-10 13:16:39 +0700

    update: uuid user

--- internal/repository/gsql/user.go
+++ internal/repository/gsql/user.go
@@ -2,16 +2,25 @@ package gsql
 
 import (
 	"lotesaleagent/model"
+	"time"
 
+	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
 
 type User struct {
-	gorm.Model
+	Id       string `gorm:"primaryKey"`
 	Username string `gorm:"unique;not null"`
 	Password string
+	CreateAt time.Time
+	UpdateAt time.Time
 }
 
+func (user *User) BeforeCreate(tx *gorm.DB) (err error) {
+	// UUID version 4
+	user.Id = uuid.NewString()
+	return
+}
 func toGormUser(entityUser *model.User) User {
 	return User{Username: entityUser.Username, Password: entityUser.Password}
 }
@@ -25,7 +34,12 @@ func NewGormUserRepository(db *gorm.DB) *GormUserRepository {
 }
 
 func (gormUser *GormUserRepository) Create(user *model.User) model.WrapError {
-	result := gormUser.db.Create(new(toGormUser(user)))
+	localUser := toGormUser(user)
+	var err = localUser.BeforeCreate(gormUser.db)
+	if err != nil {
+		return model.NewError(err)
+	}
+	result := gormUser.db.Create(&localUser)
 	if result.Error != nil {
 		return model.NewError(result.Error)
 	}
